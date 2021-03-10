cb4690b312ada3a745647544eb62a78b067360c8
Glasbleidd <mode68@gmail.com>
2021-03-11 00:35:52 +0200

    Added navigation items, menu component, and their functionality

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,38 @@
-import React from "react";
-import Aux from "../../hoc/Auxiliary";
-import classes from "./Layout.css";
+import React, { Component } from 'react';
+import Aux from '../../hoc/Auxiliary';
+import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-  <Aux>
-    <div>Toolbal, Sidedrawer, Backdrop</div>
-    <main className={classes.Content}>{props.children}</main>
-  </Aux>
-);
+class Layout extends Component {
+	state = {
+		showSideDrawer: false,
+	};
 
-export default layout;
+	sideDrawerClosedHandler = () => {
+		this.setState({
+			showSideDrawer: false,
+		});
+	};
+
+	menuClickedHandler = () => {
+		this.setState((prevState) => {
+			return { showSideDrawer: !prevState.showSideDrawer };
+		});
+	};
+
+	render() {
+		return (
+			<Aux>
+				<Toolbar menuClicked={this.menuClickedHandler} />
+				<SideDrawer
+					closed={this.sideDrawerClosedHandler}
+					open={this.state.showSideDrawer}
+				/>
+				<main className={classes.Content}>{this.props.children}</main>
+			</Aux>
+		);
+	}
+}
+
+export default Layout;
---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Auxiliary';
+
+const sideDrawer = (props) => {
+	return (
+		<Aux>
+			<Backdrop show={props.open} clicked={props.closed} />
+			<div
+				className={[
+					classes.SideDrawer,
+					props.open ? classes.Open : classes.Close,
+				].join(' ')}
+			>
+				<div className={classes.Logo}>
+					<Logo />
+				</div>
+				<nav>
+					<NavigationItems />
+				</nav>
+			</div>
+		</Aux>
+	);
+};
+
+export default sideDrawer;
