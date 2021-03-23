82e582ded5ba22e1f371e2acf7dda50d1725370d
Glasbleidd <mode68@gmail.com>
2021-03-23 16:11:44 +0200

    Outsourced checkValidity function to shared utility.js file

--- src/shared/utility.js
+++ src/shared/utility.js
@@ -4,3 +4,26 @@ export const updateObject = (oldObject, updatedProperties) => {
 		...updatedProperties,
 	};
 };
+
+export const checkValidity = (value, rules) => {
+	let isValid = true;
+
+	if (rules.required) {
+		isValid = value.trim() !== '' && isValid;
+	}
+
+	if (rules.minLength) {
+		isValid = value.length >= rules.minLength && isValid;
+	}
+
+	if (rules.maxLength) {
+		isValid = value.length <= rules.maxLength && isValid;
+	}
+
+	if (rules.isEmail) {
+		const regEx = new RegExp(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/);
+		isValid = regEx.test(value);
+	}
+
+	return isValid;
+};
