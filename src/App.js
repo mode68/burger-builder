c141cf658938b69cd2a0d28b1d1a1e5406c9b088
Glasbleidd <mode68@gmail.com>
2021-03-26 13:16:26 +0200

    Using React.lazy instead of custom asyncComponent to lazy load other components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
-import asyncComponent from './hoc/asyncComponent/asyncComponent';
 
+import Spinner from './components/UI/Spinner/Spinner';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions/index';
 
-const asyncCheckout = asyncComponent(() => {
+const Checkout = React.lazy(() => {
 	return import('./containers/Checkout/Checkout');
 });
 
-const asyncOrders = asyncComponent(() => {
+const Orders = React.lazy(() => {
 	return import('./containers/Orders/Orders');
 });
 
-const asyncAuth = asyncComponent(() => {
+const Auth = React.lazy(() => {
 	return import('./containers/Auth/Auth');
 });
 
@@ -28,7 +28,7 @@ const app = (props) => {
 
 	let routes = (
 		<Switch>
-			<Route path='/auth' component={asyncAuth} />
+			<Route path='/auth' component={Auth} />
 			<Route path='/' component={BurgerBuilder} />
 		</Switch>
 	);
@@ -36,10 +36,10 @@ const app = (props) => {
 	if (props.isAuthenticated) {
 		routes = (
 			<Switch>
-				<Route path='/checkout' component={asyncCheckout} />
-				<Route path='/orders' component={asyncOrders} />
+				<Route path='/checkout' component={Checkout} />
+				<Route path='/orders' component={Orders} />
 				<Route path='/logout' component={Logout} />
-				<Route path='/auth' component={asyncAuth} />
+				<Route path='/auth' component={Auth} />
 				<Route path='/' component={BurgerBuilder} />
 			</Switch>
 		);
@@ -48,7 +48,9 @@ const app = (props) => {
 	return (
 		<div>
 			<BrowserRouter>
-				<Layout>{routes}</Layout>
+				<Suspense fallback={<Spinner />}>
+					<Layout>{routes}</Layout>
+				</Suspense>
 			</BrowserRouter>
 		</div>
 	);
